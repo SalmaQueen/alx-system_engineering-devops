a4fba5a14db2e894b585d9675e8175b0c014fd8f
salmaQueen <salmawasee9@gmail.com>
2024-03-04 06:08:26 -0600

    moving over to cohort 17

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-# Apache returns 500
-# use this script to fix typo in config
-
-exec { 'fix apache config':
-  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
